4df1352dad77b7f110235a0a0a16086185cf9f56
Adriano Souza <148394428+Dev-Adrianoo@users.noreply.github.com>
2024-10-01 20:54:20 -0300

    Exercício loop de repetição

---
+++ ex06/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+
+  int contagem = 1;
+
+  while(contagem <= 10){
+    printf("%d\n", contagem);
+    contagem++;
+  }
+}
--- ex4/main.c
+++ ex4/main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 int main(){
+
   int d;
 
   printf("Insira um valor de 1 a 7:\n");
   scanf("%d", &d);
+
   switch(d){
     case(1):
       printf("Domingo.\n");
