751c8385832ab85954da472e6de65b1144677c53
Adriano Souza <148394428+Dev-Adrianoo@users.noreply.github.com>
2024-10-17 23:27:01 -0300

    exercise matriz

---
+++ ex33/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+  int idades[5];
+
+
+ for(int i = 0; i < 5; i++){
+    printf("informe uma idade:\n ");
+    scanf("%d", &idades[i]);
+ }
+
+
+  return 0;
+}
---
+++ ex34/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+  char cidade[15];
+
+  fgets(cidade, sizeof(cidade) ,stdin);
+  cidade[ strcspn(cidade, "\n") ] = '\0';
+  getchar();
+
+  printf("%s\n e bonito dimaisi", cidade);
+  
+
+  return 0;
+}
