d1cbdce895ba760747089921cd8a171eba422565
Adriano Souza <148394428+Dev-Adrianoo@users.noreply.github.com>
2024-10-28 13:07:10 -0300

    Exercise

--- ex49/main.c
+++ ex49/main.c
@@ -18,13 +18,19 @@ int main(){
   int matriz[3][3] = {{1,2,3},
                       {4,5,6},
                       {7,8,9}};
-  // int soma = 0;
+  int soma = 0;
 
   for(int i = 0; i < 3; i++){
     for(int j = 0; j < 3; j++){
       printf("%d ", matriz[i][j]);
     }
   }
+
+  for(int i = 0; i < 3; i++){
+    soma += matriz[i][i]; 
+  }
   
+  printf("\nSoma da diagonal da matriz: %d ", soma);
+
   return 0;
 }
