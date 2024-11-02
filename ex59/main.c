a70da175e63e7bfdb2c2271b85a9b9bd7901c5c4
Adriano Souza <148394428+Dev-Adrianoo@users.noreply.github.com>
2024-11-01 21:37:34 -0300

    Update main.c

--- ex59/main.c
+++ ex59/main.c
@@ -1,9 +1,9 @@
 /*
-  Escreva um programa em C que apresente o seguinte menu:asm
+  Escreva um programa em C que apresente o seguinte menu
   
-  1.Cadastrar conta Bancária
-  2.Realizar Depósito
-  3.Realizar saque
+  1. Cadastrar conta Bancária
+  2. Realizar Depósito
+  3. Realizar saque
   4. Listar todas as contas.
   6. Listar uma conta em específico exibindo a idade atual do titular ao invés do ano de nascimento
   7. Sair
@@ -20,6 +20,6 @@
   - CPF
 
   O programa só podera ser encerrado quando o usuário escolher a opção de sair.
-  o programa permitirá o cadastro de até 100 contas.
 
+  o programa permitirá o cadastro de até 100 contas.
 */
